4c3aed6a487b7d4302f2a81c651908ada3a82afe
Dimko <deemox@gmail.com>
2014-01-22 03:44:26 +0400

    Reduce class method calls on worker's instance in server middleware

--- lib/sidekiq-middleware/server/unique_jobs.rb
+++ lib/sidekiq-middleware/server/unique_jobs.rb
@@ -4,19 +4,20 @@ module Sidekiq
       class UniqueJobs
 
         def call(worker_instance, item, queue)
-          manual = worker_instance.class.get_sidekiq_options['manual']
+          worker_class = worker_instance.class
+          manual = worker_class.get_sidekiq_options['manual']
 
           begin
             yield
           ensure
-            clear(worker_instance, item, queue) unless manual
+            clear(worker_class, item, queue) unless manual
           end
         end
 
-        def unique_lock_key(worker_instance, item, queue)
+        def unique_lock_key(worker_class, item, queue)
           # Only enforce uniqueness across class, queue, args, and at.
           # Useful when middleware uses the payload to store metadata.
-          enabled, payload = worker_instance.class.get_sidekiq_options['unique'],
+          enabled, payload = worker_class.get_sidekiq_options['unique'],
             item.clone.slice(*%w(class queue args at))
 
           # Enabled unique scheduled
@@ -24,12 +25,17 @@ module Sidekiq
             payload.delete('at')
           end
 
-          Sidekiq::Middleware::UniqueKey.generate(worker_instance.class, payload)
+          Sidekiq::Middleware::UniqueKey.generate(worker_class, payload)
         end
 
-        def clear(worker_instance, item, queue)
-          enabled = worker_instance.class.get_sidekiq_options['unique']
-          Sidekiq.redis { |conn| conn.del unique_lock_key(worker_instance, item, queue) } if enabled
+        def clear(worker_class, item, queue)
+          enabled = worker_class.get_sidekiq_options['unique']
+
+          if enabled
+            Sidekiq.redis do |conn|
+              conn.del unique_lock_key(worker_class, item, queue)
+            end
+          end
         end
       end
     end
