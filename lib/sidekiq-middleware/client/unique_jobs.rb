12eb75bfa3461795a94fa752ba90d1a96b80d76e
Dmitry Krasnoukhov <dmitry@krasnoukhov.com>
2014-03-31 23:23:11 +0300

    Make client middleware compatible with both Sidekiq 2 and 3 Issue #25

--- lib/sidekiq-middleware/client/unique_jobs.rb
+++ lib/sidekiq-middleware/client/unique_jobs.rb
@@ -2,14 +2,13 @@ module Sidekiq
   module Middleware
     module Client
       class UniqueJobs
-        def call(worker_class, item, queue)
+        def call(worker_class, item, queue, redis_pool = nil)
           worker_class = worker_class.constantize if worker_class.is_a?(String)
           enabled = Sidekiq::Middleware::Helpers.unique_enabled?(worker_class, item)
 
           if enabled
             expiration = Sidekiq::Middleware::Helpers.unique_exiration(worker_class)
             job_id = item['jid']
-            unique = false
 
             # Scheduled
             if item.has_key?('at')
@@ -20,23 +19,33 @@ module Sidekiq
 
             unique_key = Sidekiq::Middleware::Helpers.unique_digest(worker_class, item)
 
-            Sidekiq.redis do |conn|
-              conn.watch(unique_key)
-
-              locked_job_id = conn.get(unique_key)
-              if locked_job_id && locked_job_id != job_id
-                conn.unwatch
-              else
-                unique = conn.multi do
-                  conn.setex(unique_key, expiration, job_id)
-                end
-              end
+            # Sidekiq > 3.0?
+            unique = if redis_pool
+              redis_pool.with { |conn| status(conn, unique_key, expiration, job_id) }
+            else
+              Sidekiq.redis { |conn| status(conn, unique_key, expiration, job_id) }
             end
 
             yield if unique
           else
             yield
           end
+        end
+
+        def status(conn, unique_key, expiration, job_id)
+          unique = false
+          conn.watch(unique_key)
+
+          locked_job_id = conn.get(unique_key)
+          if locked_job_id && locked_job_id != job_id
+            conn.unwatch
+          else
+            unique = conn.multi do
+              conn.setex(unique_key, expiration, job_id)
+            end
+          end
+
+          unique
         end
       end
     end
